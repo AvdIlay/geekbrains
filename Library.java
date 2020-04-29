6320e225d06faeb70110551bf510101fe21861a5
Ilya99999 <ilay963852@mail.ru>
2020-04-29 18:08:19 +0300

    J2-Lesson6

--- Library.java
+++ Library.java
@@ -5,11 +5,22 @@ public class Library {
     public static final String DELIMITER = ":";
     public static final String AUTH_REQUEST = "/auth_request";
     public static final String AUTH_ACCEPT = "/Присоединиля пользователь";
-    public static final String AUTH_DENIED = "/Не верно введен логин  или пароль";
+    public static final String AUTH_DENIED = "/auth_denied";
     public static final String MSG_FORMAT_ERROR = "/msg_format_error";
     // если мы вдруг не поняли, что за сообщение и не смогли разобрать
     public static final String TYPE_BROADCAST = "/bcast";
     // то есть сообщение, которое будет посылаться всем
+    public static final String TYPE_BCAST_CLIENT = "/client_msg";
+    public static final String USER_LIST = "/user_list";
+
+    public static String getTypeBcastClient(String msg) {
+        return TYPE_BCAST_CLIENT + DELIMITER + msg;
+    }
+
+    public static String getUserList(String users) {
+        return USER_LIST + DELIMITER + users;
+    }
+
 
     public static String getAuthRequest(String login, String password) {
         return AUTH_REQUEST + DELIMITER + login + DELIMITER + password;
@@ -31,4 +42,5 @@ public class Library {
         return TYPE_BROADCAST + DELIMITER + System.currentTimeMillis() +
                 DELIMITER + src + DELIMITER + message;
     }
+
 }
